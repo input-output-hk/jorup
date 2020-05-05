46b46a1a1a02c9e80f8cda3b70a2deff40f9b5e7
Yevhenii Babichenko <eugene.babichenko@iohk.io>
2020-05-05 20:59:48 +0300

    implement installation on windows

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -32,12 +32,18 @@ pub enum Error {
     NoInstallerExecutable(#[source] io::Error),
     #[error("Cannot install jorup in {1}")]
     Install(#[source] io::Error, PathBuf),
+    #[cfg(unix)]
     #[error("Cannot set permissions for {1}")]
     Permissions(#[source] io::Error, PathBuf),
+    #[cfg(unix)]
     #[error("Cannot read file {1}")]
     Read(#[source] io::Error, PathBuf),
+    #[cfg(unix)]
     #[error("Cannot write to file {1}")]
     Write(#[source] io::Error, PathBuf),
+    #[cfg(windows)]
+    #[error("Cannot update PATH in Windows registry")]
+    WinregError(#[source] io::Error),
 }
 
 impl Command {
@@ -69,13 +75,12 @@ pub fn install(cfg: JorupConfig, no_modify_path: bool, force: bool) -> Result<()
         }
     }
 
-    let jorup_current = std::env::current_exe().map_err(Error::NoInstallerExecutable)?;
-    std::fs::copy(&jorup_current, &jorup_file)
-        .map_err(|e| Error::Install(e, jorup_file.clone()))?;
+    let jorup_current = env::current_exe().map_err(Error::NoInstallerExecutable)?;
+    fs::copy(&jorup_current, &jorup_file).map_err(|e| Error::Install(e, jorup_file.clone()))?;
     make_executable(&jorup_file)?;
 
     if !no_modify_path {
-        do_add_to_path(&cfg, &get_add_path_methods())?;
+        do_add_to_path(&cfg)?;
     }
 
     Ok(())
@@ -89,48 +94,84 @@ pub fn update(_cfg: JorupConfig) -> Result<(), Error> {
     unimplemented!()
 }
 
-pub fn make_executable(path: &Path) -> Result<(), Error> {
-    #[cfg(windows)]
-    fn inner(_: &Path) -> Result<(), Error> {
-        Ok(())
-    }
-    #[cfg(not(windows))]
-    fn inner(path: &Path) -> Result<(), Error> {
-        use std::os::unix::fs::PermissionsExt;
+#[cfg(unix)]
+fn make_executable(path: &Path) -> Result<(), Error> {
+    use std::os::unix::fs::PermissionsExt;
 
-        let metadata = fs::metadata(path).map_err(|e| Error::Permissions(e, path.to_path_buf()))?;
-        let mut perms = metadata.permissions();
-        let mode = perms.mode();
-        let new_mode = (mode & !0o777) | 0o755;
+    let metadata = fs::metadata(path).map_err(|e| Error::Permissions(e, path.to_path_buf()))?;
+    let mut perms = metadata.permissions();
+    let mode = perms.mode();
+    let new_mode = (mode & !0o777) | 0o755;
 
-        if mode == new_mode {
-            return Ok(());
-        }
-
-        perms.set_mode(new_mode);
-        set_permissions(path, perms)
+    if mode == new_mode {
+        return Ok(());
     }
 
-    inner(path)
-}
-
-pub fn set_permissions(path: &Path, perms: fs::Permissions) -> Result<(), Error> {
+    perms.set_mode(new_mode);
     fs::set_permissions(path, perms).map_err(|e| Error::Permissions(e, path.to_path_buf()))
 }
 
-#[derive(PartialEq)]
-enum PathUpdateMethod {
-    RcFile(PathBuf),
-    Windows,
+#[cfg(windows)]
+fn make_executable(_: &Path) -> Result<(), Error> {
+    Ok(())
 }
 
-/// Decide which rcfiles we're going to update, so we
-/// can tell the user before they confirm.
-fn get_add_path_methods() -> Vec<PathUpdateMethod> {
-    if cfg!(windows) {
-        return vec![PathUpdateMethod::Windows];
+#[cfg(unix)]
+fn do_add_to_path(cfg: &JorupConfig) -> Result<(), Error> {
+    let methods = get_add_path_methods();
+
+    for rcpath in methods {
+        let file = if rcpath.exists() {
+            fs::read_to_string(&rcpath).map_err(|e| Error::Read(e, rcpath.clone()))?
+        } else {
+            String::new()
+        };
+        let addition = format!("\n{}", shell_export_string(cfg)?);
+        if !file.contains(&addition) {
+            use std::io::Write as _;
+            let mut writer = fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&rcpath)
+                .map_err(|e| Error::Write(e, rcpath.clone()))?;
+            writer
+                .write_all(addition.as_bytes())
+                .map_err(|e| Error::Write(e, rcpath.clone()))?;
+        }
     }
 
+    Ok(())
+}
+
+#[cfg(windows)]
+fn do_add_to_path(cfg: &JorupConfig) -> Result<(), Error> {
+    use winreg::enums::*;
+    use winreg::RegKey;
+
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let environment = hkcu
+        .open_subkey_with_flags("Environment", KEY_READ | KEY_WRITE)
+        .map_err(Error::WinregError)?;
+
+    let current_path: String = environment.get_value("Path").map_err(Error::WinregError)?;
+    let jorup_path = cfg.bin_dir().display().to_string();
+
+    if current_path.contains(&jorup_path) {
+        return Ok(());
+    }
+
+    let new_path = format!("{};{}", jorup_path, current_path);
+    environment
+        .set_value("Path", &new_path)
+        .map_err(Error::WinregError)?;
+
+    Ok(())
+}
+
+/// Decide which rcfiles we're going to update, so we can tell the user before
+/// they confirm.
+#[cfg(unix)]
+fn get_add_path_methods() -> Vec<PathBuf> {
     let profile = dirs::home_dir().map(|p| p.join(".profile"));
     let mut profiles = vec![profile];
 
@@ -154,45 +195,12 @@ fn get_add_path_methods() -> Vec<PathUpdateMethod> {
     }
 
     let rcfiles = profiles.into_iter().filter_map(|f| f);
-    rcfiles.map(PathUpdateMethod::RcFile).collect()
+    rcfiles.collect()
 }
 
+#[cfg(unix)]
 fn shell_export_string(cfg: &JorupConfig) -> Result<String, Error> {
     let path = cfg.bin_dir().display().to_string();
     // The path is *pre-pended* in case there are system-installed
     Ok(format!(r#"export PATH="{}:$PATH""#, path))
-}
-
-#[cfg(unix)]
-fn do_add_to_path(cfg: &JorupConfig, methods: &[PathUpdateMethod]) -> Result<(), Error> {
-    for method in methods {
-        if let PathUpdateMethod::RcFile(ref rcpath) = *method {
-            let file = if rcpath.exists() {
-                fs::read_to_string(rcpath).map_err(|e| Error::Read(e, rcpath.clone()))?
-            } else {
-                String::new()
-            };
-            let addition = format!("\n{}", shell_export_string(cfg)?);
-            if !file.contains(&addition) {
-                use std::io::Write as _;
-                let mut writer = fs::OpenOptions::new()
-                    .create(true)
-                    .append(true)
-                    .open(rcpath)
-                    .map_err(|e| Error::Write(e, rcpath.clone()))?;
-                writer
-                    .write_all(addition.as_bytes())
-                    .map_err(|e| Error::Write(e, rcpath.clone()))?;
-            }
-        } else {
-            unreachable!()
-        }
-    }
-
-    Ok(())
-}
-
-#[cfg(windows)]
-fn do_add_to_path(_cfg: &JorupConfig, _methods: &[PathUpdateMethod]) -> Result<(), Error> {
-    unimplemented!()
 }
